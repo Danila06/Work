f701b116607ffe72e1f1aa790ccfa28a8581c277
User <User@DESKTOP-GNPMGS9>
2021-12-23 22:08:18 +0300

    Добавьте файлы проекта.

---
+++ Work1/Work1/Views/AboutPageFlyoutMenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work1.Views
+{
+    public class AboutPageFlyoutMenuItem
+    {
+        public AboutPageFlyoutMenuItem()
+        {
+            TargetType = typeof(AboutPageFlyoutMenuItem);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
